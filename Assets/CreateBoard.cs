bd770c3b133e41b867d2bab2f9855a391f1e39b8
peperontino39 <peperrontino39@docomo.ne.jp>
2016-10-05 13:52:21 +0900

    盤面

---
+++ Assets/CreateBoard.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CreateBoard : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 size;
+
+
+    void Start()
+    {
+        for (int z= 0; z< size.y; z++)
+        {
+            for(int x = 0; x < size.x; x++)
+            {
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(x, 0, z);
+                int col = Convert.ToInt32((x + z)% 2 == 0);
+                cube.GetComponent<Renderer>().material.color = Color.white * col;
+                //cube.GetComponent <Color>() = new Color(col, col, col);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
