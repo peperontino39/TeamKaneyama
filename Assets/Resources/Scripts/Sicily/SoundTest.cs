584406fccb1ec145fa18614e6f6da14e97ff7806
ニコニコしん\貴之 <hime1633004@yahoo.co.jp>
2016-11-07 18:43:48 +0900

    サウンドマネージャーあげ

---
+++ Assets/Resources/Scripts/Sicily/SoundTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTest : MonoBehaviour {
+
+    public AudioClip audioClip;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+
+        SoundManager.Instance.PlayBGM(SoundManager.BGM.DADDY);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            SoundManager.Instance.PlaySE(SoundManager.SE.OUT);
+          
+        }
+	}
+}
