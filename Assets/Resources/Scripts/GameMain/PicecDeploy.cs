d3c6a3f5753931def5d81afe2fa4c03aa1b44acf
peperontino39 <peperrontino39@docomo.ne.jp>
2016-11-07 18:39:29 +0900

    上げなおし

--- Assets/Resources/Scripts/GameMain/PicecDeploy.cs
+++ Assets/Resources/Scripts/GameMain/PicecDeploy.cs
@@ -48,7 +48,7 @@ public class PicecDeploy : MonoBehaviour
         CreatePiece(PieceNum.JACK, 5, 0, 0);
 
         CreatePiece(PieceNum.KING, 4, 0, 0);
-        CreatePiece(PieceNum.JACK, 5, 4, 0);
+        //CreatePiece(PieceNum.JACK, 5, 4, 0);
 
 
         CreatePiece(PieceNum.PAWN, 0, 6, 1);
@@ -56,7 +56,7 @@ public class PicecDeploy : MonoBehaviour
         CreatePiece(PieceNum.PAWN, 2, 6, 1);
         CreatePiece(PieceNum.PAWN, 3, 6, 1);
         CreatePiece(PieceNum.PAWN, 4, 6, 1);
-        CreatePiece(PieceNum.PAWN, 5, 3, 1);
+        CreatePiece(PieceNum.PAWN, 5, 6, 1);
         CreatePiece(PieceNum.PAWN, 6, 6, 1);
         CreatePiece(PieceNum.PAWN, 7, 6, 1);
         CreatePiece(PieceNum.PAWN, 8, 6, 1);
