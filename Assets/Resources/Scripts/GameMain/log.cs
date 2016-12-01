85af0a24b36212fee3d620071af15b8acbadd137
peperontino39 <peperrontino39@docomo.ne.jp>
2016-12-01 16:33:54 +0900

    α版からちょっとだけ進んだかもしれない

---
+++ Assets/Resources/Scripts/GameMain/log.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+
+public class log : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+    int maxlog = 3;
+
+    public List<string> logTexts;
+
+    void Start()
+    {
+        text.text = "";
+
+        addText("hbjhuyg");
+        addText("hbjhuyg");
+        addText("hbjhuyg");
+        addText("hbjhuyg");
+        addText("hbjhuyg");
+        addText("hbjhuyg");
+
+    }
+
+
+    void addText(string _text)
+    {
+        logTexts.Add(_text);
+        if(logTexts.Count > maxlog)
+        {
+            logTexts.RemoveAt(maxlog);
+        }
+        string rizarut = "";
+        foreach (var ch in logTexts)
+        {
+            rizarut += ch;
+            rizarut += "\n";
+        }
+        Debug.Log(rizarut);
+        text.text = rizarut;
+
+    }
+
+}
