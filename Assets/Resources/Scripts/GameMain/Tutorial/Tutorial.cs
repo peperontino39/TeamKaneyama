7f212d3a7d7d7822f66e4c8e16f249a5c87b8eab
ニコニコしん\貴之 <hime1633004@yahoo.co.jp>
2017-01-13 17:24:31 +0900

    ためしこまわしたらごめ

---
+++ Assets/Resources/Scripts/GameMain/Tutorial/Tutorial.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject[] tutorialImage;
+
+    [SerializeField]
+    GameObject tutorialCanvas;
+
+    int imageNum;
+
+    int tutorialSwitch;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (var i in tutorialImage)
+        {
+            i.SetActive(false);
+        }
+
+
+
+        tutorialSwitch = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(imageNum);
+
+        Debug.Log(tutorialSwitch);
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            tutorialSwitch += 1;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            SetTutorialImage(imageNum + 1);
+            Debug.Log("おした");
+
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            SetTutorialImage(imageNum - 1);
+
+
+        }
+
+
+        if (tutorialSwitch >= 2)
+        {
+            tutorialSwitch = 0;
+        }
+
+
+
+        if (tutorialSwitch == 1)
+        {
+            tutorialCanvas.SetActive(true);
+            SetTutorialImage(imageNum);
+            Debug.Log("でた");
+
+        }
+        else if (tutorialSwitch == 0)
+        {
+            tutorialCanvas.SetActive(false);
+        }
+
+
+    }
+
+
+    void SetTutorialImage(int _imageNum)
+    {
+
+        tutorialImage[imageNum].SetActive(false);
+
+        imageNum = _imageNum % 10;
+        imageNum = Mathf.Max(imageNum, 0);
+
+        tutorialImage[imageNum].SetActive(true);
+
+
+
+    }
+
+
+}
+
+
+
